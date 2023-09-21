8b3273a6b7451dca6e7d2c60792180d743181b8b
JorgeCruz02 <145165020+JorgeCruz02@users.noreply.github.com>
2023-09-20 19:59:35 -0600

    Subo archivo main

---
+++ main.py
@@ -0,0 +1,11 @@
+def suma(x, y):
+    return x + y
+
+def resta(x, y):
+    return x - y
+
+def multiplicacion(x, y):
+    return x * y
+
+def is_greater_than(num_1,num_2):
+    return num_1 > num_2
