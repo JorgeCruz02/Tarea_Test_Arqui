8c1de2c0d4850c1b86ed08397cbed2e9ff1eb4d5
evavisomblain <145300707+evavisomblain@users.noreply.github.com>
2023-10-25 16:57:17 -0600

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import logging
-
 class operaciones:    
     def suma(x, y):
-        logging.info(f"Esto es un logging de suma de 2 variable {x} + {y} = {x+y}")
         return x + y
 
     def resta(x, y):
         return x - y
 
     def multiplicacion(x, y):
-        logging.info(f"Esto es un logging de multiplicacion de 2 variables {x} * {y} = {x*y}")
         return x * y
 
     def is_greater_than(num_1,num_2):
