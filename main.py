345286de1f0f9a62a8dd3f3843dbc3202e6cc9de
Alex Peña <av.5@lasallistas.org.mx>
2023-10-26 22:43:02 -0600

    loog error

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import logging
 
-logging.basicConfig(level=logging.INFO)
-console_handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(formatter)
-logger = logging.getLogger('test')
-logger.addHandler(console_handler)
-console_logs = console_handler.stream.getvalue(logger)
-print(console_logs)
+
 class operaciones:    
     def suma(x, y):
         logger.info(f"Esto es un logging de suma de 2 variable {x} + {y} = {x+y}")
