7790a0ee92c07c6b05d87a93ec37a41e580edc52
Alex Peña <av.5@lasallistas.org.mx>
2023-10-26 22:41:17 -0600

    logg error

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import logging
 
+logging.basicConfig(level=logging.INFO)
+console_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger = logging.getLogger('test')
+logger.addHandler(console_handler)
+console_logs = console_handler.stream.getvalue(logger)
+print(console_logs)
 class operaciones:    
     def suma(x, y):
-        logging.info(f"Esto es un logging de suma de 2 variable {x} + {y} = {x+y}")
+        logger.info(f"Esto es un logging de suma de 2 variable {x} + {y} = {x+y}")
         return x + y
 
     def resta(x, y):
-        return x - y
+            if x - y < 0:
+                logger.error(f"Esto es un logging de resta de 2 variable {x} - {y} = {x-y}")
 
     def multiplicacion(x, y):
         return x * y
