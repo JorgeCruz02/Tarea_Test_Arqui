58f86bcde06086be52580ce2a9f2fadd3a0bc6f5
Angelica-Cervantes2002 <68872399+Angelica-Cervantes2002@users.noreply.github.com>
2023-10-28 11:06:06 -0600

    Update test_resta.py
    Se agregaron descripciones de los logging.

--- test_resta.py
+++ test_resta.py
@@ -9,22 +9,22 @@ def test_resta():
     result = operaciones.resta(num1, num2)
 
    
-    # Logging debug
+    # Logging debug, se realiza siempre que se ejecute el código para este ejemplo.
     logging.debug('Resta realizada')
 
-    # Logging info
+    # Logging info, se realiza siempre que se ejecute el código para este ejemplo indicando el resultado de la resta. 
     logging.info(f'Resta realizada: {num1} - {num2} = {result}')
 
     if result < 0:
-        # Logging warning
+        # Logging warning, únicamente se da si el resultado es menor a cero. 
         logging.warning('El resultado es negativo, podría ocasionar algún problema en algún contexto específico.')
 
-    if result < 0:
-        # Logging error
+    if result < -5:
+        # Logging error, se da si el resultado es menor a menos cinco. 
         logging.error('La resta produjo un valor negativo, sólo se admiten resultados positivos.')
 
     if result < -10:
-        # Logging critical
+        # Logging critical, se da si el resultado es menor a menos diez. 
         logging.critical('La resta produjo un valor críticamente negativo. ¡El programa no puede continuar!')
 
     assert result == -13
